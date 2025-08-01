3074d78916747720a5e511474baba52daf29fdd8
Ravishankar Sivakumar <rsivakumar@simplesolve.india>
2025-08-01 15:47:01 +0530

    camelcase

--- Models/ViewModels/LeaveActionViewModel.cs
+++ Models/ViewModels/LeaveActionViewModel.cs
@@ -4,10 +4,10 @@ namespace LMS.Models.ViewModels
     {
         public string fullName { get; set; }
         public string designation { get; set; }
-        public int LeavePK { get; set; }
+        public int leavePK { get; set; }
         public string leaveType { get; set; }
         public string duration { get; set; }
-        public string Reason { get; set; }
+        public string reason { get; set; }
         public DateTime fromDate { get; set; }
         public DateTime toDate { get; set; }
     }
