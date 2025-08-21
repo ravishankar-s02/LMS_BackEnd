94dc2076160b5b47fbe731187f88dbeed322ca78
Ravishankar Sivakumar <rsivakumar@simplesolve.india>
2025-08-21 15:37:35 +0530

    total hours nullable

--- Models/ViewModels/LeaveUpdateModel.cs
+++ Models/ViewModels/LeaveUpdateModel.cs
@@ -7,9 +7,9 @@ namespace LMS.Models.ViewModels
         public string leaveType { get; set; }
         public DateTime fromDate { get; set; }
         public DateTime toDate { get; set; }
-        public string fromTime { get; set; }   
-        public string toTime { get; set; }     
-        public string totalHours { get; set; }
+        public string? fromTime { get; set; }   
+        public string? toTime { get; set; }     
+        public string? totalHours { get; set; }
         public string reason { get; set; }
         public string duration { get; set; }
     }
